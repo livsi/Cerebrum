577cfc85389eef928bc365b6b358c2feccc82b27
livsi <v.lezhnev@gmail.com>
2014-04-21 10:35:34 +0400

    Еще фикс деплоя

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{log pids}
+set :linked_dirs, %w{log pids public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
